ed2bc675504ee54cc090166dc77e690e818abf69
sunsidew <danielay@naver.com>
2014-07-15 15:47:57 +0900

    Initial Commit

---
+++ week1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  week1
+//
+//  Created by sunsidew on 2014. 7. 15..
+//  Copyright (c) 2014년 sunsidew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
